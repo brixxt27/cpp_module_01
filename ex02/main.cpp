f9e2ee7ea9b7d110397efd99f41a1802c31abd92
“brixxt27” <brixxt27@gmail.com>
2022-12-27 20:55:59 +0900

    feat: ex02 마무리#5
    - 출력 문자열 보기 좋게 변수에 담음
    - std::setw(), std::left 이용하여 가독성 올림
    - 함수 매개변수로 받았을 때 어떻게 될지 궁금해서 실험해 봄. 정확한
      정보는 직접 찾아서 공부할 예정(구글링, POCU) 일단 값으로는 변함이
      없고, 지금으로써는 모든 레퍼런스는 메모리 할당을 하지 않는다라고 생각
      중

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,18 +1,51 @@
+#include <iomanip>
 #include <iostream>
 
+static void test_parameter_in_function(std::string stringORG, std::string* stringPTR, std::string& stringREF) {
+	std::string		msg_add_stringORG	= "Address of stringORG: ";
+	std::string		msg_add_stringPTR	= "Address of stringPTR: ";
+	std::string		msg_add_stringREF	= "Address of stringREF: ";
+
+	std::string		msg_val_stringORG	= "Value of stringORG: ";
+	std::string		msg_val_stringPTR	= "Value of stringPTR: ";
+	std::string		msg_val_stringREF	= "Value of stringREF: ";
+
+	size_t			len_most_long_str = msg_add_stringORG.size();
+
+	std::cout << "\n** test_parameter_in_function **\n" << std::endl;
+
+	std::cout << std::setw(len_most_long_str) << std::left << msg_add_stringORG << &stringORG << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_add_stringPTR << &stringPTR << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_add_stringREF << &stringREF << std::endl;
+
+	std::cout << std::setw(len_most_long_str) << std::left << msg_val_stringORG << stringORG << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_val_stringPTR << stringPTR << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_val_stringREF << stringREF << std::endl;
+}
+
 int	main(void) {
-	std::string		str = "HI THIS IS BRAIN";
-	std::string*	stringPTR = &str;
-	std::string&	stringREF = str;
+	std::string		stringORG = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &stringORG;
+	std::string&	stringREF = stringORG;
+	
+	std::string		msg_add_stringORG	= "Address of stringORG: ";
+	std::string		msg_add_stringPTR	= "Address of stringPTR: ";
+	std::string		msg_add_stringREF	= "Address of stringREF: ";
 
-	std::cout << "Address of string: " << &str << std::endl;
-	std::cout << "Address of stringPTR: " << &stringPTR << std::endl;
-	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+	std::string		msg_val_stringORG	= "Value of stringORG: ";
+	std::string		msg_val_stringPTR	= "Value of stringPTR: ";
+	std::string		msg_val_stringREF	= "Value of stringREF: ";
 
-	std::cout << 
+	size_t			len_most_long_str = msg_add_stringORG.size();
 
-	std::cout << "Value of string: " << str << std::endl;
-	std::cout << "Value of stringPTR: " << stringPTR << std::endl;
-	std::cout << "Value of stringREF: " << stringREF << std::endl;
-	return 0;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_add_stringORG << &stringORG << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_add_stringPTR << &stringPTR << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_add_stringREF << &stringREF << std::endl;
+
+	std::cout << std::setw(len_most_long_str) << std::left << msg_val_stringORG << stringORG << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_val_stringPTR << stringPTR << std::endl;
+	std::cout << std::setw(len_most_long_str) << std::left << msg_val_stringREF << stringREF << std::endl;
+
+	test_parameter_in_function(stringORG, stringPTR, stringREF);
+	return 0;                                                                
 }
