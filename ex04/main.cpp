5c596e2dd8f346e9670ffca33b8cfae4fe6d0d0d
“brixxt27” <brixxt27@gmail.com>
2022-12-28 21:04:59 +0900

    feat: main() 예외처리#10
    - header 추가 후 예외 처리

---
+++ ex04/main.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include "no_sed.hpp"
+#include <iostream>
+
+int main(int argc, char *argvp[]) {
+    if (argc != 4) {
+        std::cout << MSG_ERR_ARGC << std::endl;
+        return 1;
+    }
+
+    
+}
